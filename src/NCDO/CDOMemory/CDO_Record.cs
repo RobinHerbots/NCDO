178c7b266b6496291ac0dbf40d958fd920b6427a
Robin Herbots <robin.herbots@gmail.com>
2018-09-14 15:24:24 +0200

    GetId fix

--- src/NCDO/CDOMemory/CDO_Record.cs
+++ src/NCDO/CDOMemory/CDO_Record.cs
@@ -84,6 +84,7 @@ namespace NCDO.CDOMemory
         /// <inheritdoc />
         public override string GetId()
         {
+            if (string.IsNullOrEmpty(primaryKey)) return _id;
             var pkValue = this.Get(primaryKey);
             return _pkValue == pkValue ? _id : pkValue.ToString();
         }
